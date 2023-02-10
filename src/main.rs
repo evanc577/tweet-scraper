b46a40e2e62031df437c2c85129f008509b635a0
Evan Chang <evanc577@gmail.com>
2023-02-10 02:26:40 -0500

    Don't panic on broken pipe

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use anyhow::Result;
+use std::io::{self, Write};
+
 use clap::Parser;
 use futures_util::stream::StreamExt;
 use tweet_scraper::TweetScraper;
 
 #[derive(Parser)]
 struct Args {
-    #[arg(short, long)]
     query: String,
 
     #[arg(short, long)]
@@ -13,19 +13,19 @@ struct Args {
 }
 
 #[tokio::main]
-async fn main() -> Result<()> {
+async fn main() {
     let args = Args::parse();
 
-    let mut scraper = TweetScraper::initialize().await?;
+    let mut scraper = TweetScraper::initialize().await.unwrap();
     let tweets_stream = scraper.tweets(args.query, args.limit).await;
     futures_util::pin_mut!(tweets_stream);
-    tweets_stream
-        .for_each(|tweet_result| {
-            let tweet = tweet_result.unwrap();
-            println!("{}", serde_json::to_string(&tweet).unwrap());
-            futures_util::future::ready(())
-        })
-        .await;
-
-    Ok(())
+    while let Some(tweet_result) = tweets_stream.next().await {
+        let tweet = tweet_result.unwrap();
+        if let Err(e) = writeln!(io::stdout(), "{}", serde_json::to_string(&tweet).unwrap()) {
+            match e.kind() {
+                io::ErrorKind::BrokenPipe => break,
+                _ => Err(e).unwrap(),
+            }
+        }
+    }
 }
